b3368c2fef483416d7c8280d8dcd14f997fe9169
Camille Protat <127672982+Starlight116@users.noreply.github.com>
2024-12-24 16:30:24 +0100

    ajout ressources

--- sidenav.js
+++ sidenav.js
@@ -12,11 +12,14 @@ function openNav() {
   }
 
 function screenSize(){
+    // If the screen is less than 800px wide, show the mobile menu
     if (window.matchMedia("(max-width: 800px)").matches) {
         document.getElementById("mySidenav").style.width = "0";
         document.getElementById("mobile").style.display = "block";
         document.getElementById("normal").style.display = "none";
+        
     } else {
+        // If the screen is greater than 800px wide, show the normal menu
         document.getElementById("mobile").style.display = "none";
         document.getElementById("normal").style.display = "block";
     }
