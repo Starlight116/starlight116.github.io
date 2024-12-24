5edbef05981fde6386f04c0a0271a66e4a998925
Camille Protat <127672982+Starlight116@users.noreply.github.com>
2024-12-24 13:35:45 +0100

    Revert "Update nav.js"
    This reverts commit 66881143ba6e7a89fe732ad96ba43c75cab90ab6.

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
 
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
