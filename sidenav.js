3c5cd011b95221e2878479376f222eeecabd0cb5
Camille Protat <protat.camille@gmail.com>
2025-01-04 18:29:28 +0100

    hero

--- sidenav.js
+++ sidenav.js
@@ -15,13 +15,26 @@ function screenSize(){
     // If the screen is less than 800px wide, show the mobile menu
     if (window.matchMedia("(max-width: 900px)").matches) {
         document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("mobile").style.display = "block";
-        document.getElementById("normal").style.display = "none";
-        
+        document.querySelectorAll("#mobile").forEach(function(element)
+        {
+            element.style.display = "flex";
+            element.style.flexDirection = "column";
+        });
+        document.querySelectorAll("#desktop").forEach(function(element)
+        {
+            element.style.display = "none";
+        });
     } else {
-        // If the screen is greater than 800px wide, show the normal menu
-        document.getElementById("mobile").style.display = "none";
-        document.getElementById("normal").style.display = "block";
+        // If the screen is greater than 800px wide, show the desktop menu
+        document.querySelectorAll("#mobile").forEach(function(element)
+        {
+            element.style.display = "none";
+        });
+        document.querySelectorAll("#desktop").forEach(function(element)
+        {
+            element.style.display = "flex";
+            element.style.flexDirection = "row";
+        });
     }
     // if i change the screen size, the menu will close
     window.addEventListener("resize", function(){
