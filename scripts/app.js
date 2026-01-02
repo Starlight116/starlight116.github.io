b22c9f3f3b2013bbb01066dc915eba11df0ab8b6
Camille Protat <protat.camille@gmail.com>
2026-01-02 18:12:03 +0100

    projets et modal ok

--- scripts/app.js
+++ scripts/app.js
@@ -28,3 +28,39 @@ window.addEventListener("hashchange", () => {
   const page = location.hash.replace("#", "") || "home";
   loadPage(page);
 });
+
+document.addEventListener("click", (e) => {
+  const btn = e.target.closest(".project-button");
+  if (!btn) return;
+
+
+  document.getElementById("overlay").classList.add("active");
+  document.body.classList.add("modal-open");
+});
+// Fermer le modal lorsque le bouton de fermeture est activé
+document.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("close")
+  ) {
+    document.getElementById("overlay").classList.remove("active");
+    document.body.classList.remove("modal-open");
+  }
+});
+
+//bouton pour retour en haut de page
+const backToTopButton = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
